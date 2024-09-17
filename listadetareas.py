062f16f2529d7fdf2318a9c0d591560e73807325
Ignacio Fleita <ignacio.fleita@alumnos.ispi4038.edu.ar>
2024-09-17 16:03:09 -0300

    3.0

--- listadetareas.py
+++ listadetareas.py
@@ -12,51 +12,52 @@ class UiListaDeTareas(object):
         if not lista_de_tareas.objectName():
             lista_de_tareas.setObjectName(u"lista_de_tareas")
         lista_de_tareas.resize(614, 331)
-        self.gridLayout = QGridLayout(lista_de_tareas)
-        self.gridLayout.setObjectName(u"gridLayout")
 
-        self.boton_eliminar = QPushButton(lista_de_tareas)
-        self.boton_eliminar.setObjectName(u"boton_eliminar")
-        self.boton_eliminar.setStyleSheet("color: #ffffff;")
-        self.gridLayout.addWidget(self.boton_eliminar, 3, 2, 1, 2)
+        self._grid_layout = QGridLayout(lista_de_tareas)
+        self._grid_layout.setObjectName(u"grid_layout")
 
-        self.label = QLabel(lista_de_tareas)
-        self.label.setObjectName(u"label")
-        self.gridLayout.addWidget(self.label, 1, 0, 1, 1)
-        self.label.setStyleSheet("border-color: #444444;")
+        self._boton_eliminar = QPushButton(lista_de_tareas)
+        self._boton_eliminar.setObjectName(u"boton_eliminar")
+        self._boton_eliminar.setStyleSheet("color: #ffffff;")
+        self._grid_layout.addWidget(self._boton_eliminar, 3, 2, 1, 2)
 
-        self.lineEdit = QLineEdit(lista_de_tareas)
-        self.lineEdit.setObjectName(u"lineEdit")
-        self.lineEdit.setPlaceholderText("Escriba su tarea...")
-        self.lineEdit.setStyleSheet("background-color: #444444; color: #ffffff;")
-        self.gridLayout.addWidget(self.lineEdit, 1, 1, 1, 1)
+        self._label = QLabel(lista_de_tareas)
+        self._label.setObjectName(u"label")
+        self._grid_layout.addWidget(self._label, 1, 0, 1, 1)
+        self._label.setStyleSheet("border-color: #444444;")
 
-        self.boton_agregar = QPushButton(lista_de_tareas)
-        self.boton_agregar.setObjectName(u"boton_agregar")
-        self.boton_agregar.setStyleSheet("color: #ffffff;")
-        self.gridLayout.addWidget(self.boton_agregar, 1, 2, 1, 1)
+        self._line_edit = QLineEdit(lista_de_tareas)
+        self._line_edit.setObjectName(u"line_edit")
+        self._line_edit.setPlaceholderText("Escriba su tarea...")
+        self._line_edit.setStyleSheet("background-color: #444444; color: #ffffff;")
+        self._grid_layout.addWidget(self._line_edit, 1, 1, 1, 1)
 
-        self.horizontalSpacer = QSpacerItem(50, 50, QSizePolicy.Expanding, QSizePolicy.Minimum)
-        self.gridLayout.addItem(self.horizontalSpacer, 3, 0, 1, 1)
+        self._boton_agregar = QPushButton(lista_de_tareas)
+        self._boton_agregar.setObjectName(u"boton_agregar")
+        self._boton_agregar.setStyleSheet("color: #ffffff;")
+        self._grid_layout.addWidget(self._boton_agregar, 1, 2, 1, 1)
 
-        self.boton_completar = QPushButton(lista_de_tareas)
-        self.boton_completar.setObjectName(u"boton_completar")
-        self.boton_completar.setStyleSheet("color: #ffffff;")
-        self.gridLayout.addWidget(self.boton_completar, 3, 1, 1, 1)
+        self._horizontal_spacer = QSpacerItem(50, 50, QSizePolicy.Expanding, QSizePolicy.Minimum)
+        self._grid_layout.addItem(self._horizontal_spacer, 3, 0, 1, 1)
 
-        self.listWidget = QListWidget(lista_de_tareas)
-        self.listWidget.setObjectName(u"listWidget")
-        self.listWidget.setStyleSheet("background-color: #333333; color: #ffffff;")
-        self.gridLayout.addWidget(self.listWidget, 2, 0, 1, 3)
+        self._boton_completar = QPushButton(lista_de_tareas)
+        self._boton_completar.setObjectName(u"boton_completar")
+        self._boton_completar.setStyleSheet("color: #ffffff;")
+        self._grid_layout.addWidget(self._boton_completar, 3, 1, 1, 1)
+
+        self._list_widget = QListWidget(lista_de_tareas)
+        self._list_widget.setObjectName(u"list_widget")
+        self._list_widget.setStyleSheet("background-color: #333333; color: #ffffff;")
+        self._grid_layout.addWidget(self._list_widget, 2, 0, 1, 3)
 
         self.interfaz(lista_de_tareas)
 
     def interfaz(self, lista_de_tareas):
         lista_de_tareas.setWindowTitle(QCoreApplication.translate("lista_de_tareas", u"Lista de Tareas", None))
-        self.boton_eliminar.setText(QCoreApplication.translate("lista_de_tareas", u"Eliminar", None))
-        self.label.setText(QCoreApplication.translate("lista_de_tareas", u"Lista de tareas", None))
-        self.boton_agregar.setText(QCoreApplication.translate("lista_de_tareas", u"Agregar tarea", None))
-        self.boton_completar.setText(QCoreApplication.translate("lista_de_tareas", u"Completada", None))
+        self._boton_eliminar.setText(QCoreApplication.translate("lista_de_tareas", u"Eliminar", None))
+        self._label.setText(QCoreApplication.translate("lista_de_tareas", u"Lista de tareas", None))
+        self._boton_agregar.setText(QCoreApplication.translate("lista_de_tareas", u"Agregar tarea", None))
+        self._boton_completar.setText(QCoreApplication.translate("lista_de_tareas", u"Completada", None))
 
 class ListaDeTareasApp(QWidget):
     def __init__(self):
@@ -64,24 +65,24 @@ class ListaDeTareasApp(QWidget):
         self.ui = UiListaDeTareas()
         self.ui.ventana_ui(self)
 
-        self.ui.boton_agregar.clicked.connect(self.agregar_tarea)
-        self.ui.boton_eliminar.clicked.connect(self.eliminar_tarea)
-        self.ui.boton_completar.clicked.connect(self.marcar_completada)
+        self.ui._boton_agregar.clicked.connect(self.agregar_tarea)
+        self.ui._boton_eliminar.clicked.connect(self.eliminar_tarea)
+        self.ui._boton_completar.clicked.connect(self.marcar_completada)
 
     def agregar_tarea(self):
-        tarea = self.ui.lineEdit.text()
+        tarea = self.ui._line_edit.text()
         if tarea:
-            self.ui.listWidget.addItem(tarea)
-            self.ui.lineEdit.clear()
+            self.ui._list_widget.addItem(tarea)
+            self.ui._line_edit.clear()
         else:
             QMessageBox.warning(self, "Advertencia", "Ingresa una tarea.")
 
     def eliminar_tarea(self):
-        for item in self.ui.listWidget.selectedItems():
-            self.ui.listWidget.takeItem(self.ui.listWidget.row(item))
+        for item in self.ui._list_widget.selectedItems():
+            self.ui._list_widget.takeItem(self.ui._list_widget.row(item))
 
     def marcar_completada(self):
-        for item in self.ui.listWidget.selectedItems():
+        for item in self.ui._list_widget.selectedItems():
             font = item.font()
             font.setStrikeOut(True)
             item.setFont(font)
@@ -91,3 +92,9 @@ if __name__ == "__main__":
     window = ListaDeTareasApp()
     window.show()
     sys.exit(app.exec())
+
+"""""
+- No hay un modelo de objetos que represente los conceptos del dominio del problema.
+- No se utiliza MVC
+- No se utiliza designer
+"""""
